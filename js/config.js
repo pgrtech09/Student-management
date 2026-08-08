87ee9352a9b7e45d858b51a22e89d8a67e572a5c
PGR TECH <pgr.tech09@gmail.com>
2026-08-08 07:11:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Vidya Campus SMS — Supabase connection config
-const SUPABASE_URL = 'https://fyfwxosupiqmllvkngua.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5Znd4b3N1cGlxbWxsdmtuZ3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NTQwNTIsImV4cCI6MjEwMTUzMDA1Mn0.1VhxugkPcDuS-BZQk-JyXCypVOvGzcj4WxPEh3bM6I4';
+const SUPABASE_URL = 'https://xdzrygsajqljjwdgxqag.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkenJ5Z3NhanFsamp3ZGd4cWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzc3MTEsImV4cCI6MjEwMTcxMzcxMX0.6h8az5C1nU3xQ15EWOJX7UY0owmakdQPmHUv6LLRE_c';
